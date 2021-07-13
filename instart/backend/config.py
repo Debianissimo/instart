c38990790bde473a0db8d99f45629ee5960887e6
Vincy <vincysuper07@gmail.com>
2021-07-13 13:57:47 -0400

    piccolo dispetto all'inghilterra ;)

--- instart/backend/config.py
+++ instart/backend/config.py
@@ -3,16 +3,16 @@ from box import Box
 
 class Config(Box):
     languages = {
+        "en-us": "Inglese (Stati Uniti)",
         "it-it": "Italiano",
         "be": "Bielorusso",
         "ca": "Catalano",
         "dz": "Arabo (Algeria)",
         "de-de": "Tedesco",
-        "es-es": "Spagnolo",
-        "en-us": "Inglese (Stati Uniti)",
         "fr-fr": "Francese",
-        "gb": "Inglese (Regno Unito)",
         "lu": "Francese (Lussemburgo)",
-        "ma": "Arabo (Morocco)",
+        "ma": "Arabo (Marocco)",
         "nl-nl": "Olandese (Paesi Bassi)",
+        "es-es": "Spagnolo",
+        "gb": "Inglese (Regno Unito) (ultimo giusto per fare un dispetto all'Inghilterra, ma per me sarebbe ultimo lo spagnolo :))",
     }
