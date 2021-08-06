b8503c7d64c0f5303c2be492e67409c099375685
Vincy <vincysuper07@gmail.com>
2021-08-06 09:31:55 -0400

    perchè secours ora vuole più spazio!?

--- instart/partitioning.py
+++ instart/partitioning.py
@@ -1,47 +1,5 @@
 #!/usr/bin/env python3
-#
-# Copyright 2019 Geoff Williams <geoff@declarativesystems.com>
-#
-# This example script is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version so long as this copyright notice remains intact.
-#
-# This example script is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this example script.  If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = """Geoff Williams <geoff@declarativesystems.com>"""
-__copyright__ = """Copyright 2019 Geoff Williams"""
-
-"""
-Allocate free space to a partition. This demo is setup for a raw image file
-`sdcard.img` which is assumed to already contain partitions but with some
-unallocated space at the end of the drive. If you have an image file that you
-have _grown_ for this purpose (eg with `dd` or `truncate`) and it is GPT, you
-need to run:
-
-  sgdisk -e FILE
-
-before running this script since GPT demands 33 backup blocks at the end of
-the disk. If you forget to do this you will get errors like this:
-
-  File "/home/geoff/.local/lib/python3.7/site-packages/parted/decorators.py", line 42, in new
-    ret = fn(*args, **kwds)
-  File "/home/geoff/.local/lib/python3.7/site-packages/parted/disk.py", line 245, in addPartition
-    constraint.getPedConstraint())
-_ped.PartitionException: Unable to satisfy all constraints on the partition.
-
-Lets grow our SD card image:
-  $ dd if=/dev/zero bs=1M count=100 >> sdcard.img
-  $ sgdisk -e sdcard.img
-"""
-
-# pip3 install pyparted
 import parted
 
 
@@ -49,7 +7,6 @@ def partition(device):
     device = parted.getDevice(device)
     disk = parted.newDisk(device)
 
-    # grab the free space region at the end of the disk
     disk.deleteAllPartitions()
 
     geometry_root = parted.Geometry(device=device, start=2048, end=32901119)
@@ -58,12 +15,11 @@ def partition(device):
     )
     geometry_swap = parted.Geometry(device=device, start=32903168, end=37029888)
     geometry_var = parted.Geometry(device=device, start=37031936, end=99946495)
-    geometry_secours = parted.Geometry(device=device, start=99944448, end=100968447)
+    geometry_secours = parted.Geometry(device=device, start=99944448, end=102041599)
     geometry_home = parted.Geometry(
-        device=device, start=100972544, end=geometry_extended.end
+        device=device, start=102043648, end=geometry_extended.end
     )
 
-    # filesystem will be shown as blank by parted until it has been _formatted_
     filesystem_root = parted.FileSystem(type="ext4", geometry=geometry_root)
     filesystem_extended = parted.FileSystem(type="ext4", geometry=geometry_extended)
     filesystem_swap = parted.FileSystem(type="linux-swap(v1)", geometry=geometry_swap)
